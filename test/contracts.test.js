00ae13331f77f3756d24255047d936ce7896b07d
vaxo-gg <vaxogig@gmail.com>
2021-04-16 02:33:09 -0700

    test scripts

--- test/contracts.test.js
+++ test/contracts.test.js
@@ -1,4 +1,4 @@
-// require("./test.Vault.js");
+require("./test.Vault.js");
 require("./test.Market.js");
 require("./test.Market2.js");
-// require("./test.Buyout.js");
+require("./test.Buyout.js");
